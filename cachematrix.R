d08038d1ff232a5a5ce0dde2905590f95c4bda48
jg9999 <john.p.galvin@intel.com>
2014-05-22 11:08:43 -0700

    Updates made to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions below are used together to improve efficiency, through caching,
+##   of matrix inversion
 
-## Write a short comment describing this function
+
+## Function: makeCacheMatrix
+## Description: creates a function containing 4 child functions
+##			set, get, setsolve, getsolve
+## Inputs: matrix - optional
+## Returns: list of available child functions
 
 makeCacheMatrix <- function(x = matrix()) {
+
+	# create a new vector to reference 
+	m <- NULL
+
+	# create the anonymous set function for the parent function
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+
+	# assigns function 'x' to 'get'
+	get <- function() x
+
+	# assigns reference of vector 'solve' to m, then to 'setsolve'
+	setsolve <- function(solve) m <<- solve
+
+	# assigns function 'm' to getsolve
+	getsolve <- function() m
+
+	# return list of available functions created above
+	list(set = set, get = get,
+		setsolve = setsolve,
+		getsolve = getsolve)	
 
 }
 
 
-## Write a short comment describing this function
+## Function: cacheSolve
+## Description: returns inverse of 'x' matrix created by 'makeCacheMatrix'
+## Inputs: matrix created by the 'makeCacheMatrix' function
+## Returns: a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	# assigns the value of getsolve in 'x' to 'm'
+	m <- x$getsolve()        
+
+	# if 'm' is not null, meaning 'x' already ran getsolve function, then
+	# return message and value of m
+	if(!is.null(m)) {
+		message("getting cached data")
+		return(m)
+	}
+
+	# get matrix
+	data <- x$get()
+	
+	# solve matrix as assigned to 'm'
+	m <- solve(data, ...)
+	
+	# assign solved 'm' back to original matrix
+	x$setsolve(m)
+	
+	# return inversed matrix
+	m
 }
